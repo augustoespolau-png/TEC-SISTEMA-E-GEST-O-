d227ed7c51e869ba2946646f1f251d5ba0a1ad7b
augustoespolau-png <augustoespolau@gmail.com>
2026-09-18 20:20:30 -0300

    fix: add mobile pinch zoom guard

---
+++ src/components/MobileZoomGuard.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function MobileZoomGuard() {
+  useEffect(() => {
+    const preventGestureZoom = (event: Event) => {
+      event.preventDefault();
+    };
+
+    const preventPinchZoom = (event: TouchEvent) => {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener("gesturestart", preventGestureZoom, {
+      passive: false,
+    });
+    document.addEventListener("gesturechange", preventGestureZoom, {
+      passive: false,
+    });
+    document.addEventListener("touchmove", preventPinchZoom, {
+      passive: false,
+    });
+
+    return () => {
+      document.removeEventListener("gesturestart", preventGestureZoom);
+      document.removeEventListener("gesturechange", preventGestureZoom);
+      document.removeEventListener("touchmove", preventPinchZoom);
+    };
+  }, []);
+
+  return null;
+}
